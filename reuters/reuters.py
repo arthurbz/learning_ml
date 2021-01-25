d36ba60cc9fcdbe1ea200da2715190ff11d1ff2a
arthurbz <arthurbziero@gmail.com>
2021-01-24 23:58:48 -0300

    reuters.py

--- reuters/reuters.py
+++ reuters/reuters.py
@@ -1,32 +1,74 @@
 from keras.datasets import reuters
+from keras import models, layers
 import numpy as np
+import matplotlib.pyplot as plt
 
 
-def vectorize_sequences(sequences, dimension=10):
-    '''results is a matrix with len(sequences) positions,
-    and every single position is an array of 10000 positions
-    where we can use 1 or 0 to know wheter a word was present
-    in the review or not
-    '''
+def vectorize_sequences(sequences, dimension=10000):
     results = np.zeros((len(sequences), dimension))
-    print(list(enumerate(sequences)))
     for i, sequence in enumerate(sequences):
-        print(i)
-        print(sequence)
-        print(list(results[i, sequence]))
         results[i, sequence] = 1
-        print(list(results[i, sequence]))
-    print(results)
     return results
 
 
-'''(train_data, train_labels), (test_data, test_labels) = \
-    reuters.load_data(num_words=10000)'''
+def to_one_hot(labels, dimension=46):
+    results = np.zeros((len(labels), dimension))
+    for i, label in enumerate(labels):
+        results[i, label] = 1
+    return results
 
-train_data = [[2, 4, 6, 7, 2, 3], [0, 2, 4, 8], [3, 6, 8], [2, 4, 6, 7]]
 
-x_train = vectorize_sequences(train_data)
-# x_test = vectorize_sequences(test_data)
+if __name__ == '__main__':
+    (train_data, train_labels), (test_data, test_labels) = \
+        reuters.load_data(num_words=10000)
 
-# print(len(train_data)) == 8982
-# print(len(test_data)) == 2246
+    x_train = vectorize_sequences(train_data)
+    x_test = vectorize_sequences(test_data)
+    one_hot_train_labels = to_one_hot(train_labels)
+    one_hot_test_labels = to_one_hot(test_labels)
+
+    model = models.Sequential()
+    model.add(layers.Dense(64, activation='relu', input_shape=(10000,)))
+    model.add(layers.Dense(64, activation='relu'))
+    model.add(layers.Dense(46, activation='softmax'))
+
+    model.compile(optimizer='rmsprop',
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+
+    x_val = x_train[:1000]
+    partial_x_train = x_train[1000:]
+    y_val = one_hot_train_labels[:1000]
+    partial_y_train = one_hot_train_labels[1000:]
+
+    history = model.fit(partial_x_train,
+                        partial_y_train,
+                        epochs=9,
+                        batch_size=512,
+                        validation_data=(x_val, y_val))
+
+    results = model.evaluate(x_test, one_hot_test_labels)
+    print(results)
+
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+    epochs = range(1, len(loss) + 1)
+
+    plt.plot(epochs, loss, 'bo', label='Training loss')
+    plt.plot(epochs, val_loss, 'b', label='Validation loss')
+    plt.title('Training on validation loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.show()
+
+    plt.clf()
+    plt.plot(epochs, acc, 'bo', label='Training acc')
+    plt.plot(epochs, val_acc, 'b', label='Validation acc')
+    plt.title('Training and validation accuracy')
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.legend()
+    plt.show()
