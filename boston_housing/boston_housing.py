9e00a50d74d3ba5e781d2f1cd83d9c4ecab00c23
arthurbz <arthurbziero@gmail.com>
2021-01-27 23:31:54 -0300

    boston_housing.py

--- boston_housing/boston_housing.py
+++ boston_housing/boston_housing.py
@@ -1,6 +1,7 @@
 from keras.datasets import boston_housing
 from keras import models, layers
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def build_model():
@@ -9,8 +10,19 @@ def build_model():
                            input_shape=(train_data.shape[1],)))
     model.add(layers.Dense(64, activation='relu'))
     model.add(layers.Dense(1))
-    model.compile(optimer='rmsprop', loss='mse', metrics=['mae'])
+    model.compile(optimizer='rmsprop', loss='mse', metrics=['mae'])
     return model
+
+
+def smooth_curve(points, factor=0.9):
+    smoothed_points = []
+    for point in points:
+        if smoothed_points:
+            previous = smoothed_points[-1]
+            smoothed_points.append(previous * factor + point * (1 - factor))
+        else:
+            smoothed_points.append(point)
+    return smoothed_points
 
 
 if __name__ == '__main__':
@@ -27,5 +39,55 @@ if __name__ == '__main__':
 
     k = 4
     num_val_samples = len(train_data) // k
-    num_epochs = 100
+    num_epochs = 500
     all_scores = []
+    all_mae_histories = []
+
+    """ for i in range(k):
+        print('processing fold #:', i)
+        val_data = train_data[i * num_val_samples: (i + 1) * num_val_samples]
+        val_targets = train_targets[i * num_val_samples: (i + 1) * num_val_samples]
+
+        partial_train_data = np.concatenate(
+            [train_data[:i * num_val_samples],
+                train_data[(i + 1) * num_val_samples:]],
+            axis=0
+        )
+        partial_train_targets = np.concatenate(
+            [train_targets[:i * num_val_samples],
+                train_targets[(i + 1) * num_val_samples:]]
+        )
+
+        model = build_model()
+        history = model.fit(partial_train_data,
+                            partial_train_targets,
+                            epochs=num_epochs,
+                            batch_size=1,
+                            verbose=0)
+        val_mse, val_mae = model.evaluate(val_data, val_targets, verbose=0)
+        all_scores.append(val_mae)
+        mae_history = history.history['mae']
+        all_mae_histories.append(mae_history) """
+
+    model = build_model()
+    history = model.fit(train_data,
+                        train_targets,
+                        epochs=80,
+                        batch_size=16,
+                        verbose=0)
+    test_mse_score, test_mae_score = model.evaluate(test_data, test_targets)
+    print('test_mse_score:', test_mse_score)
+    print('test_mae_score:', test_mae_score)
+
+    """ average_mae_history = [
+        np.mean([x[i] for x in all_mae_histories]) for i in range(num_epochs)] """
+
+    """ print('all_scores:', all_scores)
+    print('average_mae_history:', average_mae_history) """
+
+    smooth_mae_history = smooth_curve(history.history['mae'][10:])
+
+    plt.plot(range(1, len(smooth_mae_history) + 1), smooth_mae_history)
+    plt.xlabel('Epochs')
+    plt.ylabel('Validation MAE')
+    plt.show()
